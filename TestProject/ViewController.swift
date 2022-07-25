fdbb423dbbe9d89a1f40a8296fe7062a3d77e4dc
Alexey <alexeylosev1501@gmail.com>
2022-07-25 19:51:10 +0500

    Add method setupView()

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    func someFunc() {
-        
+    func setupView() {
+        view.backgroundColor = .cyan
     }
 
 
