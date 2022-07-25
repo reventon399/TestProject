a463c720c7b6625a6a050032af2a3a80cf511ac3
Alexey <alexeylosev1501@gmail.com>
2022-07-25 20:04:11 +0500

    Add setupHierarchy()

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -17,6 +17,11 @@ class ViewController: UIViewController {
     func setupView() {
         view.backgroundColor = .cyan
     }
+    
+    func setupHierarchy() {
+        let newView = UIView()
+        view.addSubview(newView)
+    }
 
 
 }
